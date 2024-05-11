e8c66b4eb60d9485a02bb3e1612a13e4b183e553
Horacio E. Valenzuela <valenzuelahoracioe@gmail.com>
2024-05-11 02:29:49 -0300

    Create project and Adding endpoint for upload photo

---
+++ bacco/bacco/views.py
@@ -0,0 +1,25 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from .models import MyModelPhoto
+import os
+
+def my_endpoint(request):
+    return HttpResponse("¡Hi world! This is baccos")
+
+
+@csrf_exempt
+def upload_photo(request):
+    if request.method == 'POST' and request.FILES.get('photo'):
+        photo = request.FILES['photo']
+        print(photo.name)
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        photo_folder_path = os.path.join(current_directory, 'photos')
+        if not os.path.exists(photo_folder_path):
+            os.makedirs(photo_folder_path)
+        photo_path = os.path.join(photo_folder_path, photo.name)
+        with open(photo_path, 'wb') as destination:
+            for chunk in photo.chunks():
+                destination.write(chunk)
+        return JsonResponse({'message': 'Foto cargada exitosamente'})
+    else:
+        return JsonResponse({'error': 'No se proporcionó ninguna foto en la solicitud'}, status=400)
