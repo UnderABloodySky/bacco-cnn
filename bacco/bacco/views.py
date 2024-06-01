694ea7ae0dc563a538e8ad12641e8f49cb1dc8f7
Horacio E. Valenzuela <valenzuelahoracioe@gmail.com>
2024-05-31 23:18:32 -0300

    Add endpoint for retrain

--- bacco/bacco/views.py
+++ bacco/bacco/views.py
@@ -26,3 +26,24 @@ def upload_photo(request):
         return JsonResponse({'message': beverage})
     else:
         return JsonResponse({'error': 'No se proporcionó ninguna foto en la solicitud'}, status=400)
+
+
+
+@csrf_exempt
+def upload_photo_for_retrain(request):
+    if request.method == 'POST' and request.FILES.get('photo') and request.FILES.get('beverage'):
+        photo = request.FILES['photo']
+        beverage = request.FILES['beverage']
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        photo_folder_path = os.path.join(current_directory, 'retrain')
+        final_path = f'{photo_folder_path}/{beverage}'
+        if not os.path.exists(final_path):
+            os.makedirs(final_path)
+        photo_path = os.path.join(final_path, photo.name)
+        with open(photo_path, 'wb') as destination:
+            for chunk in photo.chunks():
+                destination.write(chunk)
+
+        return JsonResponse({'message': "OK"}, status=200)
+    else:
+        return JsonResponse({'error': 'No se proporcionó ninguna foto en la solicitud o nombre de la bebida'}, status=400)
